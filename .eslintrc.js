fce6ea0107260280da4dd9fed159b3d8aaadd9fb
Sagar Kapoor <sagrkapr@gmail.com>
2022-12-10 06:04:52 +0200

    🙈 Removed CLF Line break rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,6 @@ module.exports = {
   },
   rules: {
     indent: ["error", 2],
-    "linebreak-style": ["error", "windows"],
     quotes: ["error", "double"],
     semi: ["error", "always"],
     eqeqeq: "error",

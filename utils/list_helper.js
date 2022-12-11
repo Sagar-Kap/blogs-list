b1442e72cf9fbde7d2865e86d9d1c8ab49f7d971
Sagar Kapoor <sagrkapr@gmail.com>
2022-12-11 14:26:14 +0200

    ✅ Completed exercise 4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -10,7 +10,20 @@ const totalLikes = (blogs) => {
   return likes;
 };
 
+const favoriteBlog = (blogs) => {
+  const sortedArr = blogs.sort((a, b) => b.likes - a.likes);
+  const largestObject = sortedArr[0];
+  console.log(largestObject);
+  const toReturn = {
+    title: largestObject.title,
+    author: largestObject.author,
+    likes: largestObject.likes,
+  };
+  return toReturn;
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };

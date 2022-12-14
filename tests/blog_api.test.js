a1fd6d25a74751e899297fea7dc88d19bf9f1378
Sagar Kapoor <sagrkapr@gmail.com>
2022-12-14 15:55:33 +0200

    ✅ Completed Ex 4.9

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -4,11 +4,21 @@ const app = require("../app");
 
 const api = supertest(app);
 
-test("Blogs are returned as json", async () => {
-  await api
-    .get("/api/blogs")
-    .expect(200)
-    .expect("Content-Type", /application\/json/);
+describe("When initially some blogs are added to db", () => {
+  test("Blogs are returned as json", async () => {
+    await api
+      .get("/api/blogs")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+  });
+
+  test("Verify the uniuque identifier property is named id", async () => {
+    const response = await api.get("/api/blogs");
+    const idArray = response.body.map((blog) => blog.id);
+    for (let i in idArray) {
+      expect(i).toBeDefined();
+    }
+  });
 });
 
 afterAll(() => {

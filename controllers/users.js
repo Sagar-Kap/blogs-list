4a91a36ce322767d3b2f815455f8b1bd00b8cc4d
Sagar Kapoor <sagrkapr@gmail.com>
2022-12-23 10:51:44 +0200

    ✅ Completed exercise 4.17

--- controllers/users.js
+++ controllers/users.js
@@ -38,7 +38,12 @@ userRouter.post("/", async (request, response) => {
 });
 
 userRouter.get("/", async (request, response) => {
-  const users = await User.find({});
+  const users = await User.find({}).populate("blogs", {
+    title: 1,
+    author: 1,
+    url: 1,
+    id: 1,
+  });
   response.json(users);
 });
 

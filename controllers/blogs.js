bd5d43f80f008c31a0eedb2c319173d9af83e815
Sagar Kapoor <sagrkapr@gmail.com>
2022-12-24 08:45:24 +0200

    ✅ Completed Exercise 4.21 bez error handling

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -4,14 +4,6 @@ const User = require("../models/user");
 const jwt = require("jsonwebtoken");
 const config = require("../utils/config");
 
-const getTokenFrom = (request) => {
-  const authorization = request.get("authorization");
-  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
-    return authorization.substring(7);
-  }
-  return null;
-};
-
 blogsRouter.get("/", async (request, response) => {
   const blogs = await Blog.find({}).populate("user", {
     username: 1,
@@ -27,7 +19,7 @@ blogsRouter.get("/", async (request, response) => {
 
 blogsRouter.post("/", async (request, response) => {
   const body = request.body;
-  const token = getTokenFrom(request);
+  const token = request.token;
   const decodedToken = jwt.verify(token, config.SECRET);
   if (!decodedToken.id) {
     return response.status(401).json({ error: "token missing or invalid" });
